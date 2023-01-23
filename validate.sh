89091b3a087417be17cc00e46a30b6b4cf9b59f9
Anna Petelin <andorjamb@protonmail.com>
2023-01-23 14:26:03 +0200

    comment out validate.sh for now

--- validate.sh
+++ validate.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Check for HTML errors
-echo "Checking HTML..."
-if ! npx htmlhint --ext .html . ; then
-  echo "HTML errors found."
-  exit 1
-fi
-echo "HTML check passed."
+#echo "Checking HTML..."
+#if ! npx htmlhint --ext .html . ; then
+#  echo "HTML errors found."
+#  exit 1
+#fi
+#echo "HTML check passed."
 
 # Check for JavaScript errors
-echo "Checking JavaScript..."
-if ! npx eslint . ; then
-  echo "JavaScript errors found."
-  exit 1
-fi
-echo "JavaScript check passed."
+#echo "Checking JavaScript..."
+#if ! npx eslint . ; then
+#  echo "JavaScript errors found."
+#  exit 1
+#fi
+#echo "JavaScript check passed."
 
-echo "Validation succeeded!"
+#echo "Validation succeeded!"
